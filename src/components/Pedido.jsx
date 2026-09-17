b1c72aeea8fb360f217e22c9cf08cc6eea566535
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-17 09:52:31 -0300

    feat: codificando as funcionalidades do pedido

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,79 +1,102 @@
 import { useState } from 'react'
 
-//Array de objetos contentdo o estado inicial do cardapio
-
+//Array de objetos contendo o estado inicial do cardápio
 const cardapio = [
-    { id: 1, nome: 'Combo 01', preco: 25.90, disponivel: true, quantidade: 0 },
-    { id: 2, nome: 'Combo 02', preco: 30.90, disponivel: true, quantidade: 0 },
-    { id: 3, nome: 'Combo 03', preco: 35.90, disponivel: true, quantidade: 0 },
+    { id: 1, nome: "Combo-01", preco: 25.00, disponivel: true, quantidade: 0 },
+    { id: 2, nome: "Combo-02", preco: 30.00, disponivel: true, quantidade: 0 },
+    { id: 3, nome: "Combo-03", preco: 35.00, disponivel: false, quantidade: 0 },
+    { id: 4, nome: "Combo-04", preco: 40.00, disponivel: true, quantidade: 0 },
 ]
 
-
 const Pedido = () => {
-    const AlterarQuantidade = (id, valor) => {
-    }
+    // HOOK- useState-Manipula o estado da variavel
+    //Exemplos vai gerenciar a listas de items do cardapio
+    const [items, setItems] = useState(cardapio);
+    const [status, setStatus] = useState("");
+    const [enviar, setEnviar] = useState(false);
 
-    //Filter: Seleciona apenas os itens que possuem quantidade maior que 0
-    const produtosDisponiveis = itens.filter(item => item.disponivel);
-    const carrinho = Items.filter(item => item.quantidade > 0)
-
-    //reduce: calcula a soma dos items (preco * quantidade) e adiciona a taxa de entrega
-    const subTotal = carrinho.reduce((acc, item) => acc + (item.preco * item.quantidade), 0) + (carrinho.length > 0 ? taxaEntrega : 0);
-    const total = subTotal > 0 ? subTotal + taxaEntrega : 0;
-
-    //SIMULACAO DO CICLO DE VIDA DA ENTREGA USANDO TEMPORIZADORES ASSINCRONOS
-
-    const confirmarPedido = () => {
-        setEnviar (true)
-        setStatus('Restaurante preparando o seu pedido')
-        setTimeout(() => {
-            setStatus('Pedido saiu para entrega')
-            setEnviar(false)
-        }, 5000)
-        setTimeout(() => {
-            setStatus('Pedido entregue com sucesso')
-            setEnviar(false)
-        }, 10000)
-    }
-        
-
-
-
-    const [itens, setItens] = useState(cardapio)
-    const [status, setStatus] = useState('')
-    const [enviar, setEnviar] = useState(false)
-
-    //valor fixo adicionado ao total quando estiver itens no carrinho
+    //valor fixo adicionado ao total quando tive items no carrinho
     const taxaEntrega = 5.00;
 
-    // funcao que altera a quantidade do pedido
-    const AlterarQuantidade = () => {
+    //função que altera a quantidade do pedido
+    const AlterarQuantidade = (id, valor) => {
         setItems(prev =>
-            // Map: percorre a lista para criar um novo array sem modificar o original
+            //MAP: percorre a lista para criar um novo array sem modificar o original
             prev.map(item =>
-                //ternario: verifica se o item da iteração atual é o que ser alterado
-                //spred(...item) adiciona o item a lista atual ou modifica
-                //Math.max: garante que a quantidade não seja negativa
-                //item: retorna o item intacto caso o id nao corresponde
+                //ternário: verifica se o item da iteração atual é o que ser alterado
+                //spread(...item) : adiciona o item a lista atual ou modifica
+                //Math.max - objeto que garante que a quantidade nunca seja menor que 0
+                //item: retorna o item intacto caso o id não corresponda
                 item.id === id ? { ...item, quantidade: Math.max(0, item.quantidade + valor) } : item
             )
         )
     }
 
+    //FILTER: Selecina apenas os produtos disponiveis e do carrinho
+    const produtosDisponiveis = items.filter(item => item.disponivel);
+    const carrinho = items.filter(item => item.quantidade > 0);
+
+    //REDUCE: Calcula a soma dos items (preco * quantidade) 
+    // e adiciona a taxa de entrega
+    const subTotal = carrinho.reduce((ac, item) => ac + item.preco * item.quantidade, 0);
+    const total = subTotal > 0 ? subTotal + taxaEntrega : 0;
+
+    //SIMULAÇÃO DO CICLO DE VIDA DA ENRTEGA USANDO TEMPORIZADORES ASSINCRONOS
+
+    const confirmarPedido = () => {
+        setEnviar(true);
+        setStatus("Restaurante Preparando seu Pedido");
+        setTimeout(() => {
+            setStatus("Seu Pedido saiu para entrega!")
+            setEnviar(false);
+        }, 5000) // 5 segundos
+        setTimeout(() => {
+            setStatus("Seu Pedido foi entregue com sucesso")
+            setEnviar(false)
+        }, 10000) // 10 segundos
+    }
+
     return (
-        <div>
-            <h1>Cardapio do Restaurante</h1>
+        <>
+            <h1>Cardápio do Restaurante</h1>
             {produtosDisponiveis.map(produto => (
                 <div key={produto.id}>
-                    <span>{produto.nome} - R$ {produto.preco.toFixed(2)}</span>
+                    <span>{produto.nome}(R${produto.preco.toFixed(2)})</span>
                     <div>
-                        <button onClick={() => AlterarQuantidade(produto.id, -1)}>-</button>
+                        <button onClick={() => { AlterarQuantidade(produto.id, -1) }} >-</button>
                         <span>{produto.quantidade}</span>
-                        <button onClick={() => AlterarQuantidade(produto.id, 1)}>+</button>
+                        <button onClick={() => AlterarQuantidade(produto.id, +1)}>+</button>
                     </div>
                 </div>
             ))}
-        </div>
+
+            <hr></hr>
+            <h3>Resumo da Entrega</h3>
+            {carrinho.length === 0 ? (
+                <p>Seu carrinho está vazio</p>
+            ) : (
+                <>
+                    <ul>
+                        {carrinho.map(item => (
+                            <li key={item.id}>
+                                {item.quantidade} x {item.nome} - R$ {item.preco * item.quantidade.toFixed(2)}
+                            </li>
+                        ))}
+                    </ul>
+                    <p>Subtotal: R$ {subTotal.toFixed(2)}</p>
+                    <p>Taxa de Entrega: R$ {taxaEntrega.toFixed(2)}</p>
+                    <button onClick={confirmarPedido} disabled={enviar} className="bg-green-400 text-white py-2 px-4 rounded">
+                        {enviar ? "Enviando Pedido..." : "Confirmar Pedido"}
+                    </button>
+
+                </>
+            )}
+            {status && (
+                <div>
+                    <strong>Status:</strong> {status}
+                </div>
+            )}
+        </>
     )
 }
 
